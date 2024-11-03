ff8e352452148b1d9d3832889ed1502bd8cde4bc
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2024-11-03 13:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (97.42%) - LeetSync

--- Multithreading/812-rotate-string/rotate-string.java
+++ Multithreading/812-rotate-string/rotate-string.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(goal.equals(s))
+        if(s.length() != goal.length())
+            return false;
+
+        String concatS = s + s;
+
+        if(concatS.indexOf(goal) != -1)
             return true;
-
-        for(int i=0; i<s.length()-1; i++){
-            String part1 = s.substring(0, i+1);
-            String part2 = s.substring(i+1);
-
-            if(goal.equals(part2+part1))
-                return true;
-        }
         return false;
     }
 }
