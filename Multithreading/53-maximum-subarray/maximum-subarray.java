1d2ee26d62980b4009ed5d98d9eb7a9961817753
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2024-11-18 23:01:11 +0530

    Time: 1 ms (99.36%) | Memory: 56.9 MB (69.89%) - LeetSync

--- Multithreading/53-maximum-subarray/maximum-subarray.java
+++ Multithreading/53-maximum-subarray/maximum-subarray.java
@@ -5,12 +5,26 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
 
+        int start=-1, end=-1, currStart=-1;
         for(int i=0; i<size; i++){
-            currSum += nums[i];
 
+            // Needed for Length of subarray or to print max subarray 
+            // if(currSum == 0)
+            //     currStart = i;
+
+            currSum += nums[i];
+            //Needed for Length of subarray or to print max subarray
+            // if(maxSum<currSum){
+            //     maxSum = currSum;
+
+            //     start = currStart;
+            //     end = i;
+            // }
             maxSum = Math.max(maxSum, currSum);
             if(currSum < 0) currSum = 0;
         }
+
+        // System.out.println("start="+start+" end="+end);
 
         return maxSum;
     }
