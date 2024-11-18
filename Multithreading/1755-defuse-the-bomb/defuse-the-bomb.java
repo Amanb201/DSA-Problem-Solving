b93f42908094cc608da59fed4ba58d5fa38c9047
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2024-11-19 01:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (37.20%) - LeetSync

--- Multithreading/1755-defuse-the-bomb/defuse-the-bomb.java
+++ Multithreading/1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
+
+        /**Brute Force: Circular Array Intuition
+        
+        
         int size = code.length;
 
         int decryptedCode[] = new int[size];
@@ -20,7 +24,7 @@ class Solution {
             }
 
             steps = k;//Irrespective of sign maintaining how much steps we have to go on left direction
-            
+
              // correctly handling modulus of negative number in circular array
             currInd = (((i-1)%size)+size)%size;
             if(currInd<0)
@@ -39,5 +43,27 @@ class Solution {
         }
 
         return decryptedCode;
+         */
+
+
+
+         //Optimized Solution from Solutions
+         int[] res = new int[code.length];
+        if (k == 0) return res;
+        //Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {//If k < 0, the starting point will be end of the array.
+            k = -k;
+            start = code.length - k;
+            end = code.length - 1;
+        }
+        for (int i = start; i <= end; i++) sum += code[i];
+        //Scan through the code array as i moving to the right, update the window sum.
+        for (int i = 0; i < code.length; i++) {
+            res[i] = sum;
+            sum -= code[(start++) % code.length];
+            sum += code[(++end) % code.length];
+        }
+        return res;
     }
 }
