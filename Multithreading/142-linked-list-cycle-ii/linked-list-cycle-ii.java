06da9dbaa03bc32311a8a5ef83b106c80f9429cb
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2024-11-19 23:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (7.95%) - LeetSync

--- Multithreading/142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ Multithreading/142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,24 +11,54 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Map<ListNode, Integer> nodeTimestamp = new HashMap<>();
-        ListNode slow = head;
-        int index = 0;
-        nodeTimestamp.put(head, index);
-        while(slow != null){
+        // Map<ListNode, Integer> nodeTimestamp = new HashMap<>();
+        // ListNode slow = head, ne;
+        // int index = 0;
+        // nodeTimestamp.put(head, index);
+        // while(slow != null){
+        //     slow = slow.next;
+
+        //     // == operator will compare the addresses of the objects
+        //     // we can also use .equals() method, by default this also compares address of the objects
+        //     // but we can override it's implementation
+        //     if(nodeTimestamp.containsKey(slow)){
+        //         return slow;
+        //     }
+
+        //     nodeTimestamp.put(slow, index);
+        //     index++;
+        // }
+
+        // return null;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-
-            // == operator will compare the addresses of the objects
-            // we can also use .equals() method, by default this also compares address of the objects
-            // but we can override it's implementation
-            if(nodeTimestamp.containsKey(slow)){
-                return slow;
-            }
-
-            nodeTimestamp.put(slow, index);
-            index++;
+            fast = fast.next.next;
+            if (slow == fast) break;
         }
+        if (fast == null || fast.next == null) return null;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
 
-        return null;
+        /**
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+        
+        
+         */
     }
 }
