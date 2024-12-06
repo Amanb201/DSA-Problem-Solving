9e5d563a897df6dee54bdc8c500ed9fe499ef003
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2024-12-06 10:36:32 +0530

    Time: 25 ms (65.59%) | Memory: 45.4 MB (47.58%) - LeetSync

--- Multithreading/1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ Multithreading/1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,4 +1,29 @@
 class Solution {
+    private int lcs(String s){
+        int sLen = s.length();
+        StringBuilder strBuilder = new StringBuilder(s);
+        String revStr = strBuilder.reverse().toString();
+
+        int[][] tabulationDp = new int[sLen+1][sLen+1];
+
+        for(int i=0; i<=sLen; i++)
+            tabulationDp[i][0] = 0;
+        for(int i=0; i<=sLen; i++)
+            tabulationDp[0][i] = 0;
+
+        for(int i=1; i<=sLen; i++){
+            for(int j=1; j<=sLen; j++){
+                if(s.charAt(i-1) == revStr.charAt(j-1)){
+                    tabulationDp[i][j] = tabulationDp[i-1][j-1]+1;
+                }
+                else{
+                    tabulationDp[i][j] = Math.max(tabulationDp[i-1][j], tabulationDp[i][j-1]);
+                }
+            }
+        }
+        return tabulationDp[sLen][sLen];
+    }
+
     private int longestPalindromeSubsequence(String s){
         int sLen = s.length();
         String sRev = "";
@@ -32,6 +57,6 @@ class Solution {
     public int minInsertions(String s) {
         int sLen = s.length();
 
-        return (sLen - longestPalindromeSubsequence(s));
+        return (sLen - lcs(s));
     }
 }
