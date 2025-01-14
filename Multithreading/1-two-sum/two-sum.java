18007fc39b7a92d484de2038c609e30ecf689d6c
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-01-14 07:38:57 +0530

    Time: 8 ms (52.03%) | Memory: 45 MB (40.55%) - LeetSync

--- Multithreading/1-two-sum/two-sum.java
+++ Multithreading/1-two-sum/two-sum.java
@@ -1,16 +1,52 @@
 class Solution {
+    class Pair{
+        int value;
+        int index;
+
+        Pair(int val, int ind){
+            this.value = val;
+            this.index = ind;
+        }
+    }
     public int[] twoSum(int[] nums, int target) {
-        int[] twoSumAns = new int[2];
+
+        //Approach-1 O(N^2)
+        // int[] twoSumAns = new int[2];
+        // for(int i=0; i<nums.length; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         if(nums[i]  + nums[j] == target){
+        //             twoSumAns[0] = i;
+        //             twoSumAns[1] = j;
+        //             return twoSumAns;
+        //         }
+        //     }
+        // }
+
+        // return twoSumAns;
+
+        Pair[] numsPair = new Pair[nums.length];
         for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]  + nums[j] == target){
-                    twoSumAns[0] = i;
-                    twoSumAns[1] = j;
-                    return twoSumAns;
-                }
-            }
+            numsPair[i] = new Pair(nums[i], i);
         }
 
+        Arrays.sort(numsPair, (a,b)->a.value-b.value);
+
+        int low =0, high = nums.length-1;
+        int[] twoSumAns = new int[2];
+
+        while(low<high){
+            if(numsPair[low].value + numsPair[high].value < target){
+                low++;
+            }
+            else if(numsPair[low].value + numsPair[high].value > target){
+                high--;
+            }
+            else{
+                twoSumAns[0] = numsPair[low].index;
+                twoSumAns[1] = numsPair[high].index;
+                return twoSumAns;
+            }
+        }
         return twoSumAns;
     }
 }
