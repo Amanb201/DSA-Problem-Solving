9caee29f70dcaa8e903585a907dff9104ae068ef
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-02-13 06:13:37 +0530

    Time: 5 ms (61.36%) | Memory: 55.8 MB (26.95%) - LeetSync

--- Multithreading/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ Multithreading/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -3,29 +3,36 @@ class Solution {
         int verticesCount = graph.length;
         int[] visited = new int[verticesCount];
         int[] pathVisited = new int[verticesCount];
+        int[] isSafeNode = new int[verticesCount];
 
         Stack<Integer> topoSortStack = new Stack<>();
+        List<Integer> safeNodes = new ArrayList<>();
+        
         for(int vertex=0; vertex<verticesCount; vertex++){
             if(visited[vertex] == 0)
-                topologicalSort(vertex, graph, visited, pathVisited, topoSortStack);
+                topologicalSort(vertex, graph, visited, pathVisited, topoSortStack, isSafeNode);
         }
 
-        List<Integer> safeNodes = new ArrayList<>();
-        while(!topoSortStack.isEmpty())
-            safeNodes.add(topoSortStack.pop());
+        for(int v=0; v<verticesCount; v++){
+            if(isSafeNode[v] == 1)
+                safeNodes.add(v);
+        }
+        //Sort the topological Sort order in ascending order the for final answer 
+        // while(!topoSortStack.isEmpty())
+        //     safeNodes.add(topoSortStack.pop());
 
-        Collections.sort(safeNodes);
+        // Collections.sort(safeNodes);
 
         return safeNodes;
     }
 
-    boolean topologicalSort(int vertex, int[][] graph, int[] visited, int[] pathVisited, Stack<Integer> topoSortStack){
+    boolean topologicalSort(int vertex, int[][] graph, int[] visited, int[] pathVisited, Stack<Integer> topoSortStack, int[] isSafeNode){
         visited[vertex] = 1;
         pathVisited[vertex] = 1;
 
         for(int adjVertex: graph[vertex]){
             if(visited[adjVertex] == 0){
-                if(topologicalSort(adjVertex, graph, visited, pathVisited, topoSortStack)){
+                if(topologicalSort(adjVertex, graph, visited, pathVisited, topoSortStack, isSafeNode)){
                     //Cycle exists i.e. Node is unsafe.
                     return true;
                 }
@@ -38,6 +45,7 @@ class Solution {
 
         pathVisited[vertex] = 0;
         topoSortStack.push(vertex);
+        isSafeNode[vertex] = 1;
         return false;
     }
 }
