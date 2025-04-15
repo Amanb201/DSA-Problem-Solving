a14c8ac3dd6e69f27521f0f60b39ab8bea754d71
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-04-15 21:04:32 +0530

    Time: 40 ms (14.65%) | Memory: 55.8 MB (82.01%) - LeetSync

--- Multithreading/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ Multithreading/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -6,32 +6,58 @@ class Solution {
 
      */
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
+        // int count = 0;
+
+        // int size = nums.length;
+        // int low =0, high = size-1;
+
+        // while(low<high){
+        //     if(nums[low] + nums[high] == k){
+        //         count++;
+        //         low++;
+        //         high--;
+                
+        //     }
+        //     else if(nums[low] + nums[high] > k){
+        //         high--;
+        //     }
+        //     else{
+        //         low++;
+        //     }
+        // }
+        // return count;
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
         int count = 0;
 
-        int size = nums.length;
-        int low =0, high = size-1;
-
-        while(low<high){
-            if(nums[low] + nums[high] == k){
+        for(int index=0; index<nums.length; index++){
+            int second = k - nums[index];
+            if(freqMap.containsKey(second) && freqMap.get(second)>0){
                 count++;
-                low++;
-                high--;
-                // if(low+1<size && nums[low+1] != nums[low]){
-                //     low++;
-                // }
-                // else{
-                //     high--;
-                // }
-            }
-            else if(nums[low] + nums[high] > k){
-                high--;
-            }
-            else{
-                low++;
+                int currFreq = freqMap.get(second);
+                freqMap.put(second, currFreq-1);
+            }else{
+                if(freqMap.containsKey(nums[index])){
+                    int currFreq = freqMap.get(nums[index]);
+                    freqMap.put(nums[index], currFreq+1);
+                }
+                else
+                    freqMap.put(nums[index], 1);
             }
         }
         return count;
+
+        /**
+    [3,1,3,4,3] k =6
+    3 - 1
+    1 - 1
+    3 - 0, 3->1
+    4 ->1
+
+
+
+     */
     }
 }
