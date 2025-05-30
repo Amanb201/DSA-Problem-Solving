0d149ec119670ff819bbb6365f99df1848d8cee9
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-05-30 08:41:49 +0530

    Time: 1 ms (99.78%) | Memory: 57.9 MB (86.07%) - LeetSync

--- Multithreading/209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ Multithreading/209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -5,23 +5,15 @@ class Solution {
 
         int left=0, right=0;
         for(right=0; right<nums.length; right++){
+            currSum += nums[right];
+           
             while(currSum>=target){
-                minLen = Math.min(minLen, right-left);
+                minLen = Math.min(minLen, right-left+1);
                 
                 currSum -= nums[left++];
             }
-            currSum += nums[right];
         }
 
-        while(currSum>=target && left<nums.length){
-            minLen = Math.min(minLen, right-left);
-            
-            currSum -= nums[left++];
-        }
         return minLen==1000000?0:minLen;
-        // for(right=right; right<nums.length; right++){
-
-        // }
-
     }
 }
