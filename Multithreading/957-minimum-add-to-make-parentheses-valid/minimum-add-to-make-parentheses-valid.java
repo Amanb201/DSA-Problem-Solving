c28eecc62315f7cde09dbb5952cd8189b4a5dc2b
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-06-07 22:01:35 +0530

    Time: 1 ms (51.21%) | Memory: 41.8 MB (29.24%) - LeetSync

--- Multithreading/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ Multithreading/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> stk = new Stack<>();
+        // Stack<Character> stk = new Stack<>();
+        int stkOpenBracket = 0;
 
         int index=0;
 
         int insertionCount = 0;
         while(index<s.length()){
             if(s.charAt(index) == '('){
-                stk.push('(');
+                // stk.push('(');
+                stkOpenBracket++;
             }
             else{
-                if(!stk.isEmpty()){
-                    stk.pop();
+                // if(!stk.isEmpty()){
+                if(stkOpenBracket>0){    
+                    // stk.pop();
+                    stkOpenBracket--;
                 }
                 else{
                     insertionCount++;
@@ -20,7 +24,9 @@ class Solution {
             index++;
         }
 
-        insertionCount += stk.size();
+        // insertionCount += stk.size();
+        insertionCount += stkOpenBracket;
+
         return insertionCount;
     }
 }
