8b3c77361bb6d1472fd991ff1308322a7fed7fef
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-07-18 22:55:07 +0530

    Time: 8 ms (21.72%) | Memory: 44.9 MB (55.77%) - LeetSync

--- Multithreading/692-top-k-frequent-words/top-k-frequent-words.java
+++ Multithreading/692-top-k-frequent-words/top-k-frequent-words.java
@@ -15,6 +15,37 @@ class Solution {
             }
         }
 
+        // return approach1(wordsFreqMap, k);
+        return optimalApproach(wordsFreqMap, k);
+    }
+
+    List<String> optimalApproach(Map<String, Integer> wordsFreqMap, int k){
+        Queue<Pair<String, Integer>> pq = new PriorityQueue<>((a,b)->{
+            return b.getValue() == a.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue();
+        });
+
+        for(String word:wordsFreqMap.keySet()){
+            if(pq.size()<k)
+                pq.add(new Pair<String, Integer>(word, wordsFreqMap.get(word)));
+            else if((pq.peek().getValue() < wordsFreqMap.get(word)) ||
+                        (pq.peek().getValue() == wordsFreqMap.get(word) && pq.peek().getKey().compareTo(word) > 0)){
+                pq.poll();
+                pq.add(new Pair<String, Integer>(word, wordsFreqMap.get(word)));
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.poll().getKey());
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+
+
+    //O(NLogN)
+    List<String> approach1(Map<String, Integer> wordsFreqMap, int k){
         Queue<Pair<String, Integer>> pq = new PriorityQueue<>((a,b)->{
             return b.getValue() == a.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue() - a.getValue();
         });
