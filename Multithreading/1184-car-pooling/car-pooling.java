3b6da58a050ca363ce9e1ea3c3e28ee646ed4d18
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-07-19 10:05:23 +0530

    Time: 6 ms (45.59%) | Memory: 44.7 MB (15.67%) - LeetSync

---
+++ Multithreading/1184-car-pooling/car-pooling.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips, (tripA, tripB)->tripA[1]-tripB[1]);
+
+        Queue<int[]> carQueue = new PriorityQueue<>((tripA,tripB)->tripA[2]-tripB[2]);
+
+        int occupied = 0;
+        for(int trip[]: trips){
+            int currPickUp = trip[1];
+            int currDropOff = trip[2];
+            
+            while(!carQueue.isEmpty() && carQueue.peek()[2]<=currPickUp){
+                int dropPassengers = carQueue.peek()[0];
+                occupied -= dropPassengers;
+                carQueue.poll();
+            }
+            
+            carQueue.add(trip);
+            occupied += trip[0];
+
+            if(occupied>capacity)
+                return false;
+        }
+        return true;
+    }
+}
