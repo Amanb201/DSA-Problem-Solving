a7249f01ab9740a0637f6fb4313932e773eb240a
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-07-20 22:41:34 +0530

    Time: 2 ms (55.48%) | Memory: 46.3 MB (26.02%) - LeetSync

--- Multithreading/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ Multithreading/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,7 +1,57 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return approach1(nums1, nums2);
+        // return approach1(nums1, nums2);
+        return optimalApproach(nums1, nums2);
     }
+
+    /**
+        Binary Search
+        Time Complexity - O(Log(N+M))
+        Space Complexity - O(1)
+     */
+    private double optimalApproach (int[] nums1, int[] nums2){
+        int arr1[], arr2[];
+
+        if(nums1.length<nums2.length){
+            arr1 = nums1; arr2 = nums2;
+        }
+        else{
+            arr1 = nums2; arr2 = nums1;
+        }
+
+        int size1 = arr1.length, size2 = arr2.length;
+        int leftPartitionSize = (size1 + size2 + 1)/2;
+
+        int low = 0, high = size1;
+        while(low<=high){
+            int mid1 = (low+high)/2;
+            int mid2 = leftPartitionSize - mid1;
+
+            int left1 = mid1-1>=0 ? arr1[mid1-1] : Integer.MIN_VALUE;
+            int left2 = mid2-1>=0 ? arr2[mid2-1] : Integer.MIN_VALUE;
+            int right1 = mid1<size1 ? arr1[mid1]: Integer.MAX_VALUE;
+            int right2 = mid2<size2 ? arr2[mid2]: Integer.MAX_VALUE;
+
+            if(left1 <= right2 && left2 <= right1){
+                //Valid Partition
+
+                //Even - Mean(Max from left Partion and Min from Right Partion)
+                //Odd - Max from left Partition
+                return (size1 + size2) % 2 == 0 ? ((double) Math.max(left1, left2) + Math.min(right1, right2))/2
+                                                : (double) Math.max(left1, left2);
+            }
+            else if(left1 > right2){
+                //Move left
+                high = mid1-1;
+            }
+            else if(left2 > right1){
+                //Move Right
+                low = mid1 + 1;
+            }
+        }
+        return -1;
+    }
+
 
     /**
         Two Pointer Approach
