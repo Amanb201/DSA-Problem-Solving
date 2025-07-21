83221eb23338554be6b66ef35c7efde0d21f4517
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-07-21 19:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (27.49%) - LeetSync

--- Multithreading/457-circular-array-loop/circular-array-loop.java
+++ Multithreading/457-circular-array-loop/circular-array-loop.java
@@ -1,5 +1,90 @@
 class Solution {
     public boolean circularArrayLoop(int[] nums) {
+        // return approach1(nums);
+        return optmialApproach(nums);
+    }
+
+    /***
+        Floyd's Cycle Detection (Tortoise and Hare) [Slow/Fast pointers]
+        Time Complexity - O(N);
+        Space Complexity - O(1);
+     */
+    private boolean optmialApproach(int[] nums){
+        int n = nums.length;
+
+        for(int i=0; i<n; i++){
+            //If Node is already visited
+            if(nums[i] == 0) continue;
+
+            int slow = i, fast = i;
+            boolean isForward = nums[i]>0;
+
+            while(true){
+                //1 Step Forward
+                slow = getNext(nums, isForward, slow);
+                //2 Step Forward
+                fast = getNext(nums, isForward, getNext(nums, isForward, fast));
+
+                //Cycle Not Possible (either self loop or Opposite Direction Move)
+                if(slow == -1 || fast == -1)    break;
+
+                //Cycle Detected [Tortoise and HARE]
+                if(slow == fast) return true;
+            }
+
+            //Mark the reachable nodes in the path starting from curr node as visited, so, that we don't have to re-visit
+            markVisited(nums, i);
+        }
+
+        //No Cycle Detected
+        return false;
+    }
+
+    private int getNext(int[] nums, boolean isForward, int currNode){
+        //Cycle not possible
+        if(currNode == -1) return -1;
+
+        boolean direction = nums[currNode]>0;
+        //Opposite direction Move not allowed in Cycle as per question
+        if(isForward != direction) return -1; 
+        //Circular Wrap
+        int next = Math.floorMod(currNode + nums[currNode], nums.length);
+
+        //Self Loop Not allowed in cycle
+        if(currNode == next)    return -1;
+        
+        return next;
+    }
+
+    private void markVisited(int[] nums, int currNode){
+        int n = nums.length;
+        boolean isForward = nums[currNode]>0;
+
+        int index = currNode;
+        while(true){
+            boolean direction = nums[index]>0;
+
+            //Opposite Direction not allowed OR if Index is already visited
+            if(isForward != direction || nums[index] == 0)
+                break;
+            //Circular Wrap
+            int next = Math.floorMod(index + nums[index], n);
+            
+            nums[index] = 0;
+            //Self Loop
+            if(index == next) break;
+
+            index = next;
+        }
+        
+    }
+
+    /***
+        DFS Cycle Detection Technique
+        Time Complexity - O(N);
+        Space Complexity - O(N);
+     */
+    private boolean approach1(int[] nums){
         int size = nums.length;
 
         //3 states required to track the cycle - Unvisited, Currently Exploring/Visiting, Visited
@@ -25,9 +110,11 @@ class Solution {
             To Handle Movement in Circular array in Left and Right Directions
 
             +ve Steps ==> (node+nums[node])%size
-            -ve Steps ==> 
+            -ve Steps ==> ((node+nums[node])%size)+size
+            for both ==> (((node+nums[node])%size)+size)%size
 
          */
+        //  int nextNode = Math.floorMod((node + nums[node]), nums.length);
         int nextNode = (((node+nums[node])%nums.length) + nums.length) % nums.length;
 
         //Self Loops/Cycle OR Cycle with moves in the same direction is allowed
