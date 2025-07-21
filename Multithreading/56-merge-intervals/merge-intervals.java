e413d88f93a6642fa9d506b6cd56917a4ef6a9ab
Aman Kumar <47862321+Amanb201@users.noreply.github.com>
2025-07-21 11:35:12 +0530

    Time: 11 ms (12.01%) | Memory: 47.8 MB (6.44%) - LeetSync

--- Multithreading/56-merge-intervals/merge-intervals.java
+++ Multithreading/56-merge-intervals/merge-intervals.java
@@ -1,4 +1,8 @@
 class Solution {
+    /**
+        Time Complexity - O(NLogN) Sorting + O(N) Iterating over each intervals once = O(NLogN)
+        Space Complexity - O(N)
+     */
     class Interval{
         int start;
         int end;
@@ -9,6 +13,47 @@ class Solution {
         }
     }
     public int[][] merge(int[][] intervals) {
+        // return mergeIntervals(intervals);
+        return practiceMerge(intervals);
+    }
+
+    public int[][] practiceMerge(int[][] intervals) {
+        Arrays.sort(intervals, (interA, interB)->interA[0] - interB[0]);
+
+        List<List<Integer>> mergedIntervals = new ArrayList<>();
+        int size = intervals.length;
+
+        for(int i=0; i<size; i++){
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+
+            int lastIndex = mergedIntervals.size()-1;
+
+            if(lastIndex<0 || mergedIntervals.get(lastIndex).get(1)<currStart){
+                mergedIntervals.add(new ArrayList<>(List.of(currStart, currEnd)));
+            }
+            else{
+                int start = mergedIntervals.get(lastIndex).get(0) < currStart 
+                                        ? mergedIntervals.get(lastIndex).get(0)
+                                        : currStart;
+
+                int end = mergedIntervals.get(lastIndex).get(1) > currEnd
+                                        ? mergedIntervals.get(lastIndex).get(1)
+                                        : currEnd;
+                mergedIntervals.set(lastIndex, new ArrayList<>(List.of(start, end)));
+            }
+        }
+
+        int mergedSize = mergedIntervals.size();
+        int mergedIntervalArr[][] = new int[mergedSize][2];
+        for(int i=0; i<mergedSize; i++){
+            mergedIntervalArr[i][0] = mergedIntervals.get(i).get(0);
+            mergedIntervalArr[i][1] = mergedIntervals.get(i).get(1);
+        }
+        return mergedIntervalArr;
+    }
+
+    private int[][] mergeIntervals(int[][] intervals) {
         int intervalSize = intervals.length;
 
         Interval[] intervalsArr = new Interval[intervalSize];
